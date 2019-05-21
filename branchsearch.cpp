e4767d19c60d415f61b82c2ce8e21a41e753f236
Nicolás Pazos <npazosmendez@gmail.com>
2019-05-20 23:52:04 -0300

    Comments

--- branchsearch.cpp
+++ branchsearch.cpp
@@ -11,14 +11,11 @@ using namespace std;
 #define MAX_BRANCH_SHOWN 20
 #define MAX_BRANCH_NAME_LENGTH 200
 
-
 #define NEW_LINE 10
 
 
-int kbhit(void)
-{
+int kbhit(void){
     int ch = getch();
-
     if (ch != ERR) {
         ungetch(ch);
         return 1;
@@ -28,6 +25,8 @@ int kbhit(void)
 }
 
 vector<string> get_branches(){
+    // naughty fix to get raw names, no origin/*
+    // depends on remote name, works for me
     string command_all_branches = "git for-each-ref --format='%(refname:short)' | sed 's/origin\\///g'";
     FILE *pp;
     vector<string> branches;
@@ -44,16 +43,18 @@ vector<string> get_branches(){
 
 void print_window(char* regex_value, vector<string*> filtered_branches, int selected_branch){
     erase();
-    initscr();
-    cbreak();
-    use_default_colors();
-    start_color();
+
+    // selection may be out of range because of new regex
     int lines = filtered_branches.size();
     if(lines && selected_branch >= lines) selected_branch = lines - 1;
+
     init_pair(1, -1, -1);
     init_pair(2, COLOR_RED, -1);
+
+    // print regex
     printw("regex: %s | %d\n\n", regex_value, lines);
-    //attrset(COLOR_PAIR(1));
+
+    // print branches
     int i = 0;
     for (; i < filtered_branches.size() && i < MAX_BRANCH_SHOWN; ++i){
         addstr("  ");
@@ -62,8 +63,9 @@ void print_window(char* regex_value, vector<string*> filtered_branches, int sele
         if(i == selected_branch) attrset(COLOR_PAIR(1));
     }
     if(i < filtered_branches.size()) addstr("...");
-    if(lines > 0) mvaddstr(selected_branch + 2, 0, ">");
 
+    // marker on selected branch
+    if(lines > 0) mvaddstr(selected_branch + 2, 0, ">");
     refresh();
 }
 
@@ -73,6 +75,7 @@ void switch_to_branch(string branch_name){
     system(command.c_str());
     exit(0);
 }
+
 void filter_branches(vector<string> &all_branches, char* regex_value, vector<string*> &filtered_branches){
     filtered_branches.clear();
     try{
@@ -89,20 +92,21 @@ void filter_branches(vector<string> &all_branches, char* regex_value, vector<str
 
 int main(int arg, char** argv)
 {
+    // setup curses
     initscr();
-
     cbreak();
     noecho();
     nodelay(stdscr, TRUE);
     curs_set(0);
     scrollok(stdscr, TRUE);
+    keypad(stdscr, TRUE);
+    use_default_colors();
+    start_color();
 
+    // variables
     char regex_value[MAX_BRANCH_NAME_LENGTH] = {0};
     int index = 0;
-    keypad(stdscr, TRUE);
-
     int selected_branch = 0;
-
     vector<string> all_branches = get_branches();
     vector<string*> filtered_branches;
     for (int i = 0; i < all_branches.size(); ++i){
@@ -111,6 +115,7 @@ int main(int arg, char** argv)
 
     print_window(regex_value, filtered_branches, selected_branch);
 
+    // handle keyboard input and loop until a branch is selected
     while (1) {
         if (kbhit()) {
             int c = getch();
@@ -126,6 +131,7 @@ int main(int arg, char** argv)
             }else if (c == KEY_DOWN){
                 if(selected_branch < filtered_branches.size() && selected_branch < MAX_BRANCH_SHOWN - 1) selected_branch++;
             }else{
+                // TODO: handle other keys
                 regex_value[index] = c;
                 index++;
             }
